9367f16737cc0afc95449db757a733d0302083d4
alexanderiksson <Alexerik69@gmail.com>
2025-05-15 17:50:52 +0200

    Added capacitor for app support

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.gymlog.app',
+  appName: 'gym-log',
+  webDir: 'dist'
+};
+
+export default config;
