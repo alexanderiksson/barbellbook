c0346a67a947e3629f7c7748287fb06dd511d339
alexanderiksson <Alexerik69@gmail.com>
2025-05-16 16:12:02 +0200

    Fix

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -4,7 +4,7 @@ import Navigation from "./components/layout/Navigation";
 export default function Layout() {
     return (
         <>
-            <div className="h-40 w-screen bg-zinc-900/50 backdrop-blur-xl fixed -top-full z-30"></div>
+            {/* <div className="h-16 w-screen bg-zinc-900/50 backdrop-blur-xl fixed top-0 z-30 border-b border-white/5"></div> */}
             <main role="main" className="flex-1 flex flex-col justify-start pt-8 pb-20">
                 <Outlet />
             </main>
