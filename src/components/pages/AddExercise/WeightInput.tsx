73185ae0eb7c0306c55c77f9cab0c3cb7d90d959
alexanderiksson <Alexerik69@gmail.com>
2025-06-26 12:13:05 +0200

    Fixed input mode

--- src/components/pages/AddExercise/WeightInput.tsx
+++ src/components/pages/AddExercise/WeightInput.tsx
@@ -10,7 +10,7 @@ export default function WeightInput({ weight, setWeight }: WeightInputProps) {
             <input
                 className="bg-zinc-800 p-2 h-12 rounded-xl w-18 border border-white/5"
                 type="number"
-                inputMode="numeric"
+                inputMode="decimal"
                 onChange={(e) => setWeight(e.target.value)}
                 value={weight}
                 step={2.5}
