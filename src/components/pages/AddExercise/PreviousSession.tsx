be4459eee53d21463bc12a1a0a2efed2b28a5851
alexanderiksson <Alexerik69@gmail.com>
2025-07-29 18:39:18 +0200

    Created component for previous session

---
+++ src/components/pages/AddExercise/PreviousSession.tsx
@@ -0,0 +1,59 @@
+import { SetType } from "../../../types/workout";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
+import { IoIosArrowDown } from "react-icons/io";
+
+interface PreviousSessionProps {
+    sets: SetType[];
+}
+
+export default function PreviousSession({ sets }: PreviousSessionProps) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <section className="bg-secondary border border-border/20 rounded-2xl p-4 text-sm">
+            <div
+                className="flex justify-between items-center cursor-pointer"
+                onClick={() => setIsOpen(!isOpen)}
+            >
+                <h2 className="text-text-grey text-sm">Previous session</h2>
+                <IoIosArrowDown
+                    size={20}
+                    color="grey"
+                    className={`transition-all ${!isOpen ? "-rotate-90" : ""}`}
+                />
+            </div>
+            <AnimatePresence initial={false}>
+                {isOpen && (
+                    <motion.div
+                        key="last-session-table"
+                        initial={{ maxHeight: 0 }}
+                        animate={{ maxHeight: 300 }}
+                        exit={{ maxHeight: 0 }}
+                        transition={{ duration: 0.3, ease: "easeInOut" }}
+                        style={{ overflow: "hidden" }}
+                    >
+                        <table className="w-full mt-4">
+                            <thead>
+                                <tr className="text-left">
+                                    <th>Set</th>
+                                    <th>Reps</th>
+                                    <th>Weight</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {sets.map((set, index) => (
+                                    <tr key={index}>
+                                        <td>Set {index + 1}</td>
+                                        <td>{set.reps}</td>
+                                        <td>{set.weight}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </motion.div>
+                )}
+            </AnimatePresence>
+        </section>
+    );
+}
