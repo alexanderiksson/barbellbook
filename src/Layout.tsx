8ff95fb160420582337c1225875d69be192ce750
alexanderiksson <Alexerik69@gmail.com>
2025-09-02 09:58:24 +0200

    Nav fix

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -4,7 +4,7 @@ import Navigation from "./components/layout/Navigation";
 export default function Layout() {
     return (
         <>
-            <div className="hidden standalone:block fixed w-full top-0 standalone:h-18 border-b border-border/20 bg-background/50 backdrop-blur-xl z-30"></div>
+            <div className="hidden standalone:block fixed w-full top-0 standalone:h-16 border-b border-border/20 bg-secondary-bright/50 backdrop-blur-xl z-30"></div>
             <main
                 role="main"
                 className="flex-1 flex flex-col justify-start pt-8 pb-20 lg:pb-8 lg:ml-52 xl:ml-60 standalone:pt-24 standalone:pb-26"
