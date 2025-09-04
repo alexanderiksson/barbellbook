7aec07ed7edd7b84511ec63b5fcf3959b0a8ec18
alexanderiksson <Alexerik69@gmail.com>
2025-09-04 16:36:07 +0200

    Added search field

--- src/pages/Exercises.tsx
+++ src/pages/Exercises.tsx
@@ -1,15 +1,18 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { useWorkout } from "../context/WorkoutContext";
 import { Link } from "react-router-dom";
 import exercisesData from "../data/exercises.json";
 
 import PageHeading from "../components/common/PageHeading";
 import BackButton from "../components/common/BackButton";
+import { TextInput } from "../components/common/Inputs";
 
 import { IoIosArrowForward } from "react-icons/io";
 
 export default function Exercises() {
     const { workouts } = useWorkout();
+
+    const [search, setSearch] = useState("");
 
     // Find the all exercises sorted by most logged
     const allExercises = useMemo(() => {
@@ -32,14 +35,32 @@ export default function Exercises() {
             });
     }, [workouts]);
 
+    // Search field
+    const filteredExercises = useMemo(() => {
+        if (!search) return allExercises;
+        return allExercises.filter((exercise) =>
+            exercise.name.toLowerCase().includes(search.toLowerCase())
+        );
+    }, [allExercises, search]);
+
     return (
         <div className="content">
             <BackButton to="/stats" label="Stats" />
             <PageHeading>Exercises</PageHeading>
 
+            <div className="mb-8">
+                {/* Search field */}
+                <TextInput
+                    placeholder="Search exercise..."
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                />
+                {/* Category filter */}
+            </div>
+
             <section>
                 <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
-                    {allExercises.map((exercise, index) => (
+                    {filteredExercises.map((exercise, index) => (
                         <Link key={index} to={`/stats/exercises/${exercise.id}`}>
                             <div className="flex justify-between items-center gap-4 p-3 bg-secondary rounded-xl border border-border/20">
                                 <h3 className="truncate">
