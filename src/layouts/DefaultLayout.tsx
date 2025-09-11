2c013a2b16c178ab33f7e552183f8c0c8333aeed
alexanderiksson <Alexerik69@gmail.com>
2025-09-11 14:25:50 +0200

    Increased top padding

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -10,7 +10,7 @@ export default function DefaultLayout() {
                 role="main"
                 className="flex-1 flex flex-col justify-start lg:ml-52 xl:ml-60"
                 style={{
-                    paddingTop: "calc(1rem + var(--safe-top))",
+                    paddingTop: "calc(1.5rem + var(--safe-top))",
                     paddingBottom: "calc(5rem + var(--safe-bottom))",
                 }}
             >
